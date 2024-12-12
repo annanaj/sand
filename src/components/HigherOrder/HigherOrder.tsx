f1e6abb682f4f17ca678495bd1644c6b44610c26
Anna <65922372+annanaj@users.noreply.github.com>
2024-12-12 13:42:40 +0100

    Fix types for HOC

--- src/components/HigherOrder/HigherOrder.tsx
+++ src/components/HigherOrder/HigherOrder.tsx
@@ -1,16 +1,24 @@
 import React, { useState } from 'react';
 
-const HigherOrder = (WrappedComponent) => {
-	return function WithCounterComponent(props) {
+type HigherOrderProps = {
+	onBuy?: () => void;
+};
+
+const HigherOrder = <P extends object>(
+	WrappedComponent: React.ComponentType<P>
+) => {
+	return function WithCounterComponent(props: P & HigherOrderProps) {
 		const [count, setCount] = useState(0);
 
 		const handleIncrement = () => {
 			setCount((prevCount) => prevCount + 1);
 		};
 
+		const onBuy = props.onBuy || handleIncrement;
+
 		return (
 			<div className="card-container items-center">
-				<WrappedComponent {...props} onBuy={handleIncrement} />
+				<WrappedComponent {...props} onBuy={onBuy} />
 				<div className="flex items-center space-x-4 pt-2">
 					<span>counter: {count}</span>
 				</div>
