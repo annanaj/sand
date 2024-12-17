4896ad6fadb876fae7421f48df23fed1157c6664
Anna <65922372+annanaj@users.noreply.github.com>
2024-12-17 13:34:40 +0100

    Add useEffect example for window width measurement

--- src/components/Keyframes/Keyframes.tsx
+++ src/components/Keyframes/Keyframes.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ImSpinner9 } from 'react-icons/im';
 import { Button } from '@/components/ui/button';
 import Spinner from '../svg/spinner.svg';
 import styles from './Keyframes.module.scss';
 
 export default function Keyframes() {
+	const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+	// useEffect pro sledovani sirky okna a nasledne logiky
+	useEffect(() => {
+		const handleResize = () => {
+			setWindowWidth(window.innerWidth);
+		};
+		// pridani event listeneru
+		window.addEventListener('resize', handleResize);
+		// cleanup funkce při unmountu komponenty
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
+	// prazdny dependency array tj effect probehne jen pri 1.renderu
+	// zmenu zajistuje sam handler handleresize, kdybysme dali do deps treba windowWidth
+	// byl by to loop infinite
+
 	return (
 		<div className="card-container items-center">
 			<h2 className="title">Keyframes</h2>
+			<p className="pb-8">Window width with useEffect: {windowWidth}px</p>
 			<div className="flex gap-3 mt-5">
 				<div
 					className={`${styles.floatingCircle} ${styles.first}`}
